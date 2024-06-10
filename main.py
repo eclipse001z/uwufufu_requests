e78ef2794eda5f17f339a5cf64ac5ba50303d1bb
eclipse <166458255+eclipse001z@users.noreply.github.com>
2024-06-10 06:39:52 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,29 @@
 import asyncio
 import aiohttp
 from fake_useragent import UserAgent
-from colorama import Fore, Back, Style
+from colorama import Fore, Back, Style, init
+
+init()
 
 game_id = input("Game ID: ")
 
 
 print ("\nPerson to win alot/boost in rankings")
-winner = input("Winner ID: ") # winner
+winner = input("Winner ID: ") 
 print ("\n Person to lose alot/tank in rankings\n")
 tank = input("Tank ID: ") # Person to lose alot 
 print("\n")
-amm = int(input("Amount Of Boosts: ")) # amount of boosts
+amm = int(input("Amount Of Boosts: "))
 ua = UserAgent()
 
 headers = {
     'User-Agent': ua.random,
     'Content-Type': 'application/json'
 }
-
+boosts_given = 0
 async def start_game(session, payload):
+    global boosts_given
+
     async with session.post("https://production-api.uwufufu.com/v1/worldcup/startedGames", headers=headers, json=payload) as response:
         if response.status == 201:
             data = await response.json()
@@ -31,14 +35,15 @@ async def start_game(session, payload):
                 "startedGameId": startedGameId,
                 "selections": [ #64359d3ebf2461aba7f6ff83
                     {"_id": tank, "wins": False},
-                    {"_id": winner, "wins": True} 
+                    {"_id": winner, "wins": True} #valk
                 ]
             }
 
             async with session.post("https://production-api.uwufufu.com/v1/worldcup/startedGames/pick", headers=headers, json=pick) as pick_response:
                 # print(pick_response.status)
                 # print(await pick_response.json())
-                print(f"{Fore.BLACK}Succesfully Boosted [{Fore.WHITE+"+1"+Fore.BLACK}]")
+                boosts_given += 1
+                print(f"Boosts Given: [{Fore.LIGHTBLACK_EX}{str(boosts_given)}{Fore.RESET}]")
         else:
             print("Error starting the game:", await response.json())
 
